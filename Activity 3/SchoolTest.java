68e852b90a6728614310b713f2b1800ed21e4f85
Kazukiro <tuludkaje@gmai.com>
2025-04-15 22:29:09 +0800

    Initial commit with Person class and related files

--- Activity 3/SchoolTest.java
+++ Activity 3/SchoolTest.java
@@ -1,28 +1,5 @@
 import java.util.Scanner;
 
-class Person {
-    private String name;
-    private int id;
- 
-    public Person(String name, int id) {
-        this.name = name;
-        this.id = id;
-    }
- 
-    public String getName() {
-        return this.name;
-    }
-   
-    public int getID() {
-        return this.id;
-    }
- 
-    public void displayInfo() {
-        System.out.println("Your name: " + name);
-        System.out.println("Your ID: " + id);
-    }
-}
-
 public class SchoolTest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
